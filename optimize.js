dc5ccd043f79217e91f4f662f87529e3354a140b
Yu@macbook-pro <jvyu59jvyu@icloud.com>
2020-06-02 15:32:44 +0800

    Remove http-proxy package

--- optimize.js
+++ optimize.js
@@ -1,10 +1,6 @@
 const express = require('express');
-const httpproxy = require('http-proxy');
-
-const EXPENSIVE_SERVER = process.env.EXPENSIVE_SERVER || 'http://localhost:8088';
 
 const app = express();
-const proxyor = httpproxy.createProxyServer({});
 
 app.get('/expensive', (req, res) => {
   res.send('Block this api.')
@@ -13,14 +9,5 @@ app.get('/expensive', (req, res) => {
 app.get('/optimize', (req, res) => {
   res.send('This is an optimize api.')
 })
-
-app.all('/*', (req, res) => {
-  proxyor.web(req, res, {
-    target: EXPENSIVE_SERVER,
-    toProxy: true,
-    headers: req.headers,
-    proxyTimeout: 5000
-  });
-});
 
 app.listen(process.env.PORT || 8087)
